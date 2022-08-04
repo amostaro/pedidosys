71139286247c971e1c4f09dfd553c6202df8912b
amostaro <amostaro@ciandt.com>
2022-08-04 16:56:44 -0300

    commons, utils, userMicroservices and database create

--- src/main/java/com/totalshake/pedidosys/PedidosysApplication.java
+++ src/main/java/com/totalshake/pedidosys/PedidosysApplication.java
@@ -1,13 +1,21 @@
 package com.totalshake.pedidosys;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PedidosysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosysApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
 	}
 
 }
