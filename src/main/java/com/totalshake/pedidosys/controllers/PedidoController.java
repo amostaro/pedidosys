80571df9d85ba1c2f25a3efa5b651ea7303a7b44
amostaro <amostaro@ciandt.com>
2022-08-05 08:52:33 -0300

    retrieveAllPedidos

--- src/main/java/com/totalshake/pedidosys/controllers/PedidoController.java
+++ src/main/java/com/totalshake/pedidosys/controllers/PedidoController.java
@@ -1,7 +1,25 @@
 package com.totalshake.pedidosys.controllers;
 
+import com.totalshake.pedidosys.models.Pedido;
+import com.totalshake.pedidosys.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/pedidos")
 public class PedidoController extends BaseController {
+
+    @Autowired
+    PedidoService pedidoService;
+
+    @GetMapping("/retrieve-all")
+    public ResponseEntity<List<Pedido>> retrieveAllPedidos() {
+        List<Pedido> pedidosList = pedidoService.retrieveAllPedidos();
+        return ResponseEntity.ok(pedidosList);
+    }
 }
--- src/main/java/com/totalshake/pedidosys/services/PedidoService.java
+++ src/main/java/com/totalshake/pedidosys/services/PedidoService.java
@@ -1,7 +1,19 @@
 package com.totalshake.pedidosys.services;
 
+import com.totalshake.pedidosys.models.Pedido;
+import com.totalshake.pedidosys.repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PedidoService extends BaseService {
+
+    @Autowired
+    PedidoRepository pedidoRepository;
+    public List<Pedido> retrieveAllPedidos() {
+        List<Pedido> pedidosList = pedidoRepository.findAll();
+        return pedidosList;
+    }
 }
